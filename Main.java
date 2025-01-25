f5fa3ac20a2100b8b3e1bc1fab18d5e2d4603785
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-01-26 02:58:06 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+// public class oops{
+//     int p = 10;
+//     public static void main(String args[]){
+//         oops piyush = new oops();
+//         System.out.println(piyush.p);
+//     }
+// }
+
+
+// public class oops{
+//     int p = 39;
+//     int k1 = 78;
+//     public static void main(String args[]){
+//         oops piyush = new oops();
+//         oops piyush1 = new oops();
+//         System.out.println(piyush.p);
+//         System.out.println(piyush1.k1);
+//     }
+// }
+
+// public class oops{
+//     int a = 20;
+//     public static void main(String rags[]){
+//         oops myobj = new oops();
+//         myobj.a = 40;
+//         System.out.println(myobj.a);
+//     }
+// }
+
+public class oops{
+    int y= 10;
+    public static void main(String args[]){
+        oops piyush1 = new oops();
+        oops piyush2 = new oops();
+        piyush2.y= 30;
+        System.out.println(piyush1.y);
+        System.out.println(piyush2.y);
+    }
+}
